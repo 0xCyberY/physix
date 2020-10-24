2e7666ce5ea64320a34c7d24dc489b6693aee7e5
Tree Davies <tdavies@darkphysics.net>
2020-10-24 16:24:16 -0700

    Fixup 3.080-grub/build.sh to do the write thing with respect to CONF_SKIP_PARTITIONING
    modified:   build-groups/03-base-config/3.080-grub/build.sh
    modified:   physix.conf
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- build-groups/03-base-config/3.080-grub/build.sh
+++ build-groups/03-base-config/3.080-grub/build.sh
@@ -13,34 +13,48 @@ ROOT_PART='\/dev\/'$ROOT_PART
 KERNEL=`ls /boot | grep vmlinuz-`
 INITRD=`ls /boot | grep initrd-`
 
-if [ $CONF_UEFI_ENABLE == "n" ] ; then
-	# This might fail due to Error:'will not proceed with blocklists'
-	# Can be forced with --force
-	grub-install --target=i386-pc --force /dev/$ROOT_DEV
-	chroot_check $? "grub-install /dev/$ROOT_DEV"
-else
-	grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=PHYSIX --force /dev/$ROOT_DEV
-	chroot_check $? "grub-install EFI"
+if [ $CONF_SKIP_PARTITIONING == "n" ]  ; then
+	# IF PARTITIONING WAS NOT SKIPPED, IT SHOULD BE SAFE TO INSTALL GRUB
+	if [ $CONF_UEFI_ENABLE == "n" ] ; then
+		# This might fail due to Error:'will not proceed with blocklists'
+		# Can be forced with --force
+		grub-install --target=i386-pc --force /dev/$ROOT_DEV
+		chroot_check $? "grub-install /dev/$ROOT_DEV"
+	else
+		grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=PHYSIX --force /dev/$ROOT_DEV
+		chroot_check $? "grub-install EFI"
+	fi
 fi
 
-install --verbose --mode 644 --owner root --group root $PKG_DIR_PATH/lvm-grub.cfg  /boot/grub/grub.cfg
+install --verbose --mode 644 --owner root --group root $PKG_DIR_PATH/lvm-grub.cfg  /boot/grub/physix-grub.cfg
 chroot_check $? "Install grub.cfg"
 
 SED_CMD='s/SET_ROOT_MARKER/'$SET_ROOT'/g'
-sed -i $SED_CMD /boot/grub/grub.cfg
-chroot_check $? "Grub sed edit $SED_CMD grub.cfg"
+sed -i $SED_CMD /boot/grub/physix-grub.cfg
+chroot_check $? "Grub sed edit $SED_CMD physix-grub.cfg"
 
 SED_CMD='s/VOL_GROUP_MARKER/'$CONF_VOL_GROUP_NAME'/g'
-sed -i $SED_CMD /boot/grub/grub.cfg
-chroot_check $? "Grub sed edit $SED_CMD grub.cfg"
+sed -i $SED_CMD /boot/grub/physix-grub.cfg
+chroot_check $? "Grub sed edit $SED_CMD physix-grub.cfg"
 
 SED_CMD='s/KERNEL_MARKER/'$KERNEL'/g'
-sed -i $SED_CMD /boot/grub/grub.cfg
-chroot_check $? "Grub sed edit $SED_CMD grub.cfg"
+sed -i $SED_CMD /boot/grub/physix-grub.cfg
+chroot_check $? "Grub sed edit $SED_CMD physix-grub.cfg"
 
 SED_CMD='s/INITRD_MARKER/'$INITRD'/g'
-sed -i $SED_CMD /boot/grub/grub.cfg
-chroot_check $? "Grub sed edit $SED_CMD grub.cfg"
+sed -i $SED_CMD /boot/grub/physix-grub.cfg
+chroot_check $? "Grub sed edit $SED_CMD physix-grub.cfg"
+
+if [ $CONF_SKIP_PARTITIONING == "n" ] ; then
+	# IF PARTITIONING WAS NOT SKIPPED, WE ASSUME /boot WAS 
+    # CREATED AND FORMATTED BY PHYSIX, AND THUS IS SAFE TO WRITE grub.cfg
+	if [ ! -e /boot/grub/grub.cfg ] ; then
+		mv /boot/grub/physix-grub.cfg /boot/grub/grub.cfg
+		chroot_check $? "mv /boot/grub/physix-grub.cfg /boot/grub/grub.cfg"
+	else
+		echo "Found /boot/grub/grub.cfg, so will not over-write"
+	fi
+fi
 
 if [ -e /boot/grub ] ; then
 	install --verbose --mode 444 --owner root --group root $PKG_DIR_PATH/unicode.pf2  /boot/grub/fonts/
